3e55494dc0df87b4c6d04dc710d53f5006af8e63
AhmedHosny2024 <eng.ahmedhosny2024@gmail.com>
2024-04-02 04:30:51 +0200

    update folder structure

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { JwtStrategy } from './jwt.strategy';
-import { UserModule } from '../modules/user/user.module';
+import { UserModule } from '../user/user.module';
 
 
 
